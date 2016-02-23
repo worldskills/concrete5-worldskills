04f035178ba5df4fdaac3a338483cac18c4e4601
Fabian Vogler <fabian@equivalence.ch>
2016-02-23 22:01:59 +0100

    Increase version number

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'worldskills';
     protected $appVersionRequired = '5.7.3.1';
-    protected $pkgVersion = '0.9.3';
+    protected $pkgVersion = '0.9.4';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderMapCoreExtensions = true;
 
