a43908d233b96c8f5d1d0063f59043c68d817145
Fabian Vogler <fabian@equivalence.ch>
2016-03-02 15:13:22 +0100

    Remove thumbnail class from member list

--- blocks/worldskills_member_list/view.php
+++ blocks/worldskills_member_list/view.php
@@ -25,22 +25,19 @@ $nh = \Loader::helper('navigation');
             ?>
 
             <div class="col-sm-2">
-                <?php if ($url): ?>
-                    <a href="<?php echo h($url); ?>" class="thumbnail">
-                <?php else: ?>
-                    <div class="thumbnail">
-                <?php endif; ?>
-                <?php if (isset($member['flag']) && $member['flag']): ?>
-                    <img src="<?php echo h($member['flag']['thumbnail']); ?>_small" class="img-responsive" alt="" style="border: 1px solid #ddd; max-height: 84px;">
-                <?php endif; ?>
-                <div class="caption">
-                    <h5><?php echo h($member['name']['text']); ?></h5>
-                </div>
-                <?php if ($url): ?>
-                    </a>
-                <?php else: ?>
-                    </div>
-                <?php endif; ?>
+                <p>
+                    <?php if ($url): ?>
+                        <a href="<?php echo h($url); ?>">
+                    <?php endif; ?>
+                    <?php if (isset($member['flag']) && $member['flag']): ?>
+                        <img src="<?php echo h($member['flag']['thumbnail']); ?>_small" class="img-responsive" alt="" style="border: 1px solid #ddd; max-height: 84px;">
+                        <br>
+                    <?php endif; ?>
+                    <?php echo h($member['name']['text']); ?>
+                    <?php if ($url): ?>
+                        </a>
+                    <?php endif; ?>
+                </p>
             </div>
 
         <?php endforeach; ?>
