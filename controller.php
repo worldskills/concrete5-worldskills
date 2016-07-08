7b89183bdc6c34acd543a9de77a172b6dc0a78cd
Fabian Vogler <fabian@equivalence.ch>
2016-07-08 14:54:38 +0200

    Version 0.9.15

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'worldskills';
     protected $appVersionRequired = '5.7.3.1';
-    protected $pkgVersion = '0.9.14';
+    protected $pkgVersion = '0.9.15';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderMapCoreExtensions = true;
 
