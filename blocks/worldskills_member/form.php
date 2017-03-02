3f11f098574ae5d17fd72eab177be7a030c81ff5
Fabian Vogler <fabian@equivalence.ch>
2017-03-02 10:40:56 +0100

    Fix Member block edit

--- blocks/worldskills_member/form.php
+++ blocks/worldskills_member/form.php
@@ -7,7 +7,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
     <div class="input">
         <select name="memberId" id="memberId" class="form-control">
             <?php foreach ($members as $member): ?>
-                <option value="<?php echo h($member['id']); ?>"<?php if ($parentId == $member['id']) { echo ' selected="selected"'; } ?>>
+                <option value="<?php echo h($member['id']); ?>"<?php if ($memberId == $member['id']) { echo ' selected="selected"'; } ?>>
                     <?php echo h($member['name']['text']); ?>
                 </option>
             <?php endforeach; ?>
