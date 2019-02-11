10e32f6d67bee921fc22fb65ae0c10c0c8e5d318
Fabian Vogler <fabian@equivalence.ch>
2019-02-11 10:51:53 +0100

    Add WorldSkills profile link

---
+++ authentication/worldskills/hooked.php
@@ -0,0 +1,6 @@
+<div class="form-group">
+    <a href="https://people.worldskills.org/" class="btn btn-primary" target="_blank">
+        <i class="fa fa-user"></i>
+        <?= t('WorldSkills profile') ?>
+    </a>
+</div>
