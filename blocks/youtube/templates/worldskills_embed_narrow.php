140673e9bf0acf34ba41dceaa9ae4efc9cbd78cc
Fabian Vogler <fabian@equivalence.ch>
2019-03-12 14:36:02 +0100

    Update narrow YouTube template

--- blocks/youtube/templates/worldskills_embed_narrow.php
+++ blocks/youtube/templates/worldskills_embed_narrow.php
@@ -1,9 +1,12 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+?>
 
 <div class="row">
 
-    <div class="col-lg-10 offset-lg-1">
+    <div class="col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
 
-        <?php require dirname(__FILE__) . '/worldskills_embed_condensed.php'; ?>
+        <?php require dirname(__FILE__) . '/worldskills_embed.php'; ?>
 
     </div>
 
