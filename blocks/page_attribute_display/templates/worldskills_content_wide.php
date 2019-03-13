8f84af8f918391c9d8a60fbe2e91e666126625fc
Fabian Vogler <fabian@equivalence.ch>
2019-03-13 08:41:20 +0100

    Add wide template for Page Attribute Display block

---
+++ blocks/page_attribute_display/templates/worldskills_content_wide.php
@@ -0,0 +1,13 @@
+<?php
+defined('C5_EXECUTE') or die('Access Denied.');
+?>
+
+<div class="row">
+
+    <div class="col-xl-10 offset-xl-1">
+
+        <?php require dirname(__FILE__) . '/worldskills_content.php'; ?>
+
+    </div>
+
+</div>
