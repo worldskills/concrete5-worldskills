922b1c38558159ecb67ac7dda12b67a98fa168e2
hengkysanjaya123 <hengkysanjaya204@gmail.com>
2021-04-09 20:46:07 +0700

    Upgrade $pkgVersion to 1.8.0

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'worldskills';
     protected $appVersionRequired = '5.7.3.1';
-    protected $pkgVersion = '1.7.1';
+    protected $pkgVersion = '1.8.0';
     protected $pkgAllowsFullContentSwap = true;
     protected $pkgAutoloaderMapCoreExtensions = true;
 
